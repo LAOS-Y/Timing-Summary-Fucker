185b663e822821546640ad68dd9925393efbe806
LAOS-Y <billyang426@gmail.com>
2019-06-22 20:27:13 +0800

    add argparser

--- fuck_report.py
+++ fuck_report.py
@@ -1,14 +1,15 @@
 import re
 import random
+import argparse
 
-def getFucker(min_scale=0.75, max_scale=1.25, gauss_mean=1, gauss_std=0.15):
+def getFucker(min_scale=0.75, max_scale=1.25, mean_scale=1, std_scale=0.15):
     pat = re.compile('[0-9]+\.[0-9][0-9][0-9]')
     
     def fuckLine(line):
         found = re.findall(pat, line)
         
         for num in found:
-            new_num = float(num) * (max(min_scale, min(max_scale, random.gauss(gauss_mean, gauss_std))))
+            new_num = float(num) * (max(min_scale, min(max_scale, random.gauss(mean_scale, std_scale))))
             new_num = '%.3f' % new_num
             line = line.replace(num, new_num)
         
@@ -16,18 +17,35 @@ def getFucker(min_scale=0.75, max_scale=1.25, gauss_mean=1, gauss_std=0.15):
     
     return fuckLine
 
-path = 'timing_report.txt'
-output_path = 'new_report.txt'
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="original timing report path")
+parser.add_argument("-o", "--output", default=None, help="output path")
 
-fuck = getFucker()
+parser.add_argument("-m", "--min-scale", type=float, default=0.75, help="min zoom scale")
+parser.add_argument("-M", "--max-scale", type=float, default=1.25, help="max zoom scale")
+parser.add_argument("--mean", type=float, default=1.25, help="mean of zoom scale")
+parser.add_argument("--std", type=float, default=1.25, help="standard deviation of zoom scale")
 
-in_file = open(path)
-out_file = open(output_path, mode='w')
+args = parser.parse_args()
+
+assert args.min_scale < args.max_scale, "min_scale should be smaller than max_scale. Got min_scale: {}, max_scale: {}".format(args.min_scale, args.max_scale)
+
+fuck = getFucker(args.min_scale, args.max_scale, args.mean, args.std)
+
+in_file = open(args.path)
+
+if args.output is not None:
+    out_file = open(args.output, mode='w')
 
 for line in in_file:
     line = fuck(line.strip())
-    print(line, file=out_file)
-    out_file.flush()
+    if args.output is not None:
+        print(line, file=out_file)
+        out_file.flush()
+    else:
+        print(line)
 
 in_file.close()
-out_file.close()
+
+if args.output is not None:
+    out_file.close()
